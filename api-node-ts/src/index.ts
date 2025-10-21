1a4f4fa8b52110f9b308653c73278e46b8cbd475
MiniPoinManjaro <gxluck444@gmail.com>
2025-10-21 14:55:22 +0200

    TD6 TypeScript / NodeJS

---
+++ api-node-ts/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv'; 
+import userRoutes from './routes/user.routes'; 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.get('/', (req: Request, res: Response) => {
+res.send('🚀 API Node.js avec TypeScript fonctionne !'); 
+});
+app.use('/users', userRoutes);
+app.listen(PORT, () => {
+	console.log(`✅ Serveur démarré sur http://localhost:${PORT}`);
+});
